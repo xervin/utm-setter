0f92b50fccf9fb0117d9a623c2dc95e0595ed851
xervin <42388676+xervin@users.noreply.github.com>
2022-02-09 16:46:02 +0300

    Update utmSetter.js

--- src/utmSetter.js
+++ src/utmSetter.js
@@ -1,47 +1,43 @@
 (function() {
-    const utmSetterInstance = () => {
-        class UtmSetter {
-            init() {
-                const utm = [];
-                (new URLSearchParams(window.location.search)).forEach((v, i) => {
-                    let kv = i+'='+v;
-                    if (i.indexOf('utm_') === 0 && !utm.includes(kv)) {
-                        utm.push(kv)
-                    }
-                })
-                let replace = utm.join('&');
-                if (replace.length) {
-                    document.querySelectorAll('[*|href]').forEach((el) => {
-                        this.addUTM(el, replace)
-                    });
+    class UtmSetter {
+        init() {
+            const utm = [];
+            (new URLSearchParams(window.location.search)).forEach((v, i) => {
+                let kv = i+'='+v;
+                if (i.indexOf('utm_') === 0 && !utm.includes(kv)) {
+                    utm.push(kv)
                 }
-            }
-
-            addUTM(el, replace) {
-                if (typeof el.href === 'object') {
-                    let xlinkHref = el.getAttributeNS('http://www.w3.org/1999/xlink', 'href');
-                    if (xlinkHref.startsWith('http')) {
-                        el.setAttributeNS(
-                            'http://www.w3.org/1999/xlink',
-                            'xlink:href',
-                            xlinkHref + (xlinkHref.indexOf('?') === -1 ? '?' : '&') + replace
-                        );
-                    }
-                } else {
-                    if (el.href.startsWith('http')) {
-                        el.href = el.href + (el.href.indexOf('?') === -1 ? '?' : '&') + replace;
-                    }
-                }
+            })
+            let replace = utm.join('&');
+            if (replace.length) {
+                document.querySelectorAll('[*|href]').forEach((el) => {
+                    this.addUTM(el, replace)
+                });
             }
         }
 
-        return new UtmSetter()
+        addUTM(el, replace) {
+            if (typeof el.href === 'object') {
+                let xlinkHref = el.getAttributeNS('http://www.w3.org/1999/xlink', 'href');
+                if (xlinkHref.startsWith('http')) {
+                    el.setAttributeNS(
+                        'http://www.w3.org/1999/xlink',
+                        'xlink:href',
+                        xlinkHref + (xlinkHref.indexOf('?') === -1 ? '?' : '&') + replace
+                    );
+                }
+            } else {
+                if (el.href.startsWith('http')) {
+                    el.href = el.href + (el.href.indexOf('?') === -1 ? '?' : '&') + replace;
+                }
+            }
+        }
     }
 
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = utmSetterInstance()
+        module.exports = UtmSetter
     } else {
-        window.UtmSetter = utmSetterInstance()
+        window.UtmSetter = UtmSetter
     }
 }());
 
